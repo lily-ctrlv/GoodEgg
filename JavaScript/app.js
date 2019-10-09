2518959498ebbbe549fe3f55275798fc71f2c909
Liliana Martins <lilianasrmartins@gmail.com>
2019-10-09 19:04:13 +0100

    refactoring
    added smooth scrolling
    added more readable comments to js file
    removed unecessary stylesheet

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,22 +1,29 @@
 //navbar scrolling colour-changine effect
 
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $('.navbar').css({
+        'background': 'white',
+        'opacity': '0.8'
+      });
+      $('.nav-item').css('color', 'black');
+      $('.navbar .navbar-brand img').attr('src', 'Media/EGG-LOGO-black.png').css({
+        'width': '100px',
+        'height': 'auto'
+      });
+    } else {
+      $('.navbar').css({
+        'background': 'transparent'
+      });
+      $('.nav-link').css('color', 'white');
+      $('.navbar .navbar-brand img').attr('src', 'Media/EGG-LOGO-white.png').css({
+        'width': '100px',
+        'height': 'auto'
+      });
 
-
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 300) {
-	    $('.navbar').css({'background':'white','opacity':'0.8'});
-      $('.nav-item').css('color','black');
-      $('.navbar .navbar-brand img').attr('src','Media/EGG-LOGO-black.png').css({'width':'100px', 'height':'auto'});
-	  }
-
-	  else {
-		  $('.navbar').css({'background':'transparent'});
-      $('.nav-link').css('color','white');
-      $('.navbar .navbar-brand img').attr('src','Media/EGG-LOGO-white.png').css({'width':'100px', 'height':'auto'});
-
-	  }
+    }
   })
 })
 
@@ -24,45 +31,35 @@ $(document).ready(function(){
 //smooth scrolling
 
 // Select all links with hashes
-$(document).ready(function(){
+$(document).ready(function() {
 
   // Get the height of the navbar
   var navbarHeight = $("nav").height();
 
- // Add smooth scrolling to all links
- $("a").on('click', function(event) {
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-   if (this.hash !== "") {
-     // Prevent default anchor click behavior
-     event.preventDefault();
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-     // Store hash
-     var hash = this.hash;
-     var target = $(this).attr("href"); //Get the target
-     var scrollToPosition = $(target).offset().top - (navbarHeight + 50);
+      // Store hash
+      var hash = this.hash;
+      var target = $(this).attr("href"); //Get the target
+      var scrollToPosition = $(target).offset().top - (navbarHeight + 50);
 
+    //actual animation using pre-set var - this works really well if you change the css of your element it will automatically capture that info
 
-
-     // Using jQuery's animate() method to add smooth page scroll
-     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-
-     //method 1
-     // $('html, body').animate({
-     //   scrollTop: $(hash).offset().top-navbarHeight}, 800, function(){
-     //
-     //   window.location.hash = hash;
-
-
-
-     //method 2
-
-     $('html').animate({ 'scrollTop': scrollToPosition }, 600, function(){
-    window.location.hash = "" + target;
-    // This hash change will jump the page to the top of the div with the same id
-    // so we need to force the page to back to the end of the animation
-    $('html').animate({ 'scrollTop': scrollToPosition }, 0);
-});
-     };
-
- });
+      $('html').animate({
+        'scrollTop': scrollToPosition
+      }, 600, function() {
+        window.location.hash = "" + target;
+        // This hash change will jump the page to the top of the div with the same id
+        // so we need to force the page to back to the end of the animation
+        $('html').animate({
+          'scrollTop': scrollToPosition
+        }, 0);
+      });
+    };
+  });
 });
